367d704ca8564529f8cb12c53009e165f6debfe8
radekkrejcirik01 <radek.krejcirik@cloudtalk.io>
2022-08-07 16:18:14 +0200

    Animatable change to Lottie

--- src/components/swipe/Swiper/Swiper.style.ts
+++ src/components/swipe/Swiper/Swiper.style.ts
@@ -4,10 +4,6 @@ export const SwiperStyle = StyleSheet.create({
     container: {
         flex: 1
     },
-    animatableView: {
-        position: 'absolute',
-        alignSelf: 'center'
-    },
     viewPager: {
         width: '100%',
         height: '100%'
--- src/components/swipe/Swiper/Swiper.tsx
+++ src/components/swipe/Swiper/Swiper.tsx
@@ -1,24 +1,26 @@
-import React, { useMemo, useState } from 'react';
-import { StyleProp, View, ViewStyle } from 'react-native';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import ViewPager from '@react-native-community/viewpager';
-import * as Animatable from 'react-native-animatable';
+import LottieView from 'lottie-react-native';
 import { SwiperCard } from '@components/swipe/SwiperCard/SwiperCard';
 import {
     CardDataProps,
     SwiperProps
 } from '@components/swipe/Swiper/Swiper.props';
 import { SwiperStyle } from '@components/swipe/Swiper/Swiper.style';
-import { IconEnum } from '@components/icon/Icon.enum';
-import { Icon } from '@components/icon/Icon';
 import { usePullToRefresh } from '@hooks/usePullToRefresh';
 import { Modal } from '@components/general/Modal/Modal';
 import { useModal } from '@hooks/useModal';
 import { InfoProfileScreen } from '@screens/general/InfoProfileScreen/InfoProfileScreen';
 import { InfoProps } from '@screens/general/InfoProfileScreen/InfoProfileScreen.props';
+import { AnimatedLottieViewInterface } from '@components/swipe/SwiperCard/SwiperCard.props';
 
 export const Swiper = ({ data }: SwiperProps): JSX.Element => {
     const { name } = data[0];
+
+    const lottieRef = useRef<AnimatedLottieViewInterface>(null);
+
     const [info, setInfo] = useState<InfoProps>(null);
 
     const { top } = useSafeAreaInsets();
@@ -32,11 +34,6 @@ export const Swiper = ({ data }: SwiperProps): JSX.Element => {
     } = usePullToRefresh(name);
 
     const { modalVisible, showModal, hideModal } = useModal();
-
-    const paddingTop = useMemo(
-        (): StyleProp<ViewStyle> => ({ paddingTop: top || 10 }),
-        [top]
-    );
 
     const modalContent = useMemo(
         (): JSX.Element => (
@@ -54,16 +51,39 @@ export const Swiper = ({ data }: SwiperProps): JSX.Element => {
         showModal();
     };
 
+    useEffect(() => {
+        if (isAnimation) {
+            lottieRef.current.reset(0); // TODO: useLottie
+            lottieRef.current.play(2, 75);
+            return;
+        }
+        lottieRef.current.reset(0);
+    }, [isAnimation]);
+
     return (
         <View style={SwiperStyle.container}>
-            {isAnimation && (
-                <Animatable.View
-                    animation="bounceIn"
-                    style={[SwiperStyle.animatableView, paddingTop]}
-                >
-                    <Icon name={IconEnum.FLASH_FILLED} size={38} />
-                </Animatable.View>
-            )}
+            <View
+                style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'absolute',
+                    alignSelf: 'center',
+                    top
+                }}
+            >
+                <LottieView
+                    ref={lottieRef}
+                    source={require('../../../assets/animations/animation.json')}
+                    autoPlay={false}
+                    loop={false}
+                    style={[
+                        {
+                            width: 50,
+                            height: 50
+                        }
+                    ]}
+                />
+            </View>
             <ViewPager
                 orientation="vertical"
                 initialPage={0}
