c4a4e2de7e157fe157058bf18334e18d8d83e341
radekkrejcirik01 <radek.krejcirik@cloudtalk.io>
2022-11-06 20:49:25 +0100

    refactor: add useProfileScrollView

--- src/components/profile/ProfileScrollView/ProfileScrollView.tsx
+++ src/components/profile/ProfileScrollView/ProfileScrollView.tsx
@@ -1,123 +1,19 @@
-import React, {
-    useCallback,
-    useEffect,
-    useMemo,
-    useRef,
-    useState
-} from 'react';
-import {
-    Animated,
-    NativeScrollEvent,
-    NativeSyntheticEvent
-} from 'react-native';
-import { useDispatch } from 'react-redux';
-import { setBottomBarVisible } from '@store/BottomBarReducer';
+import React from 'react';
+import { Animated } from 'react-native';
 import { ProfileScrollViewProps } from '@components/profile/ProfileScrollView/ProfileScrollView.props';
 import { ProfileAnimatedImage } from '@components/profile/ProfileAnimatedImage/ProfileAnimatedImage';
-import { animatedEventConfig } from '@components/profile/ProfileScrollView/ProfileScrollView.config';
-import { interpolateConfig } from '@components/profile/ProfileAnimatedImage/ProfileAnimatedImage.config';
-import AnimatedInterpolation = Animated.AnimatedInterpolation;
+import { useProfileScrollView } from '@hooks/useProfileScrollView';
 
 export const ProfileScrollView = ({
     source,
     children
 }: ProfileScrollViewProps): JSX.Element => {
-    const dispatch = useDispatch();
-
-    const [lastContentOffset, setLastContentOffset] = useState<number>(0);
-    const [isScrolling, setIsScrolling] = useState<boolean>(false);
-    const [isBottomBarVisible, setIsBottomBarVisible] = useState<boolean>(true);
-    const [canImageInterpolate, setCanImageInterpolate] =
-        useState<boolean>(false);
-
-    const scrollY = useRef(new Animated.Value(0)).current;
-    const listRef = useRef(null);
-
-    useEffect(() => {
-        dispatch(setBottomBarVisible(isBottomBarVisible));
-    }, [dispatch, isBottomBarVisible]);
-
-    const isScrollingDown = useCallback(
-        (offset: number, y: number) => offset > y,
-        []
-    );
-
-    const onScroll = useMemo(
-        (): ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) =>
-            Animated.event(
-                [
-                    {
-                        nativeEvent: {
-                            contentOffset: { y: scrollY }
-                        }
-                    }
-                ],
-                {
-                    ...animatedEventConfig,
-                    listener: (
-                        event: NativeSyntheticEvent<NativeScrollEvent>
-                    ) => {
-                        const isDown = isScrollingDown(
-                            lastContentOffset,
-                            event.nativeEvent.contentOffset.y
-                        );
-
-                        if (
-                            isDown &&
-                            event.nativeEvent.contentOffset.y <= 0 &&
-                            canImageInterpolate &&
-                            !isScrolling
-                        ) {
-                            listRef.current?.scrollTo({
-                                x: 0,
-                                y: 0,
-                                animated: false
-                            });
-                            setCanImageInterpolate(false);
-                        }
-
-                        if (isDown && event.nativeEvent.contentOffset.y < 150) {
-                            setIsBottomBarVisible(true);
-                        } else if (
-                            !isDown &&
-                            event.nativeEvent.contentOffset.y >= 0 &&
-                            isScrolling
-                        ) {
-                            setIsBottomBarVisible(false);
-                        }
-
-                        if (
-                            !isDown &&
-                            event.nativeEvent.contentOffset.y > 100
-                        ) {
-                            setCanImageInterpolate(true);
-                        }
-
-                        setLastContentOffset(event.nativeEvent.contentOffset.y);
-                    }
-                }
-            ),
-        [
-            canImageInterpolate,
-            isScrolling,
-            isScrollingDown,
-            lastContentOffset,
-            scrollY
-        ]
-    );
-
-    const onScrollBeginDrag = () => setIsScrolling(true);
-
-    const onScrollEndDrag = () => setIsScrolling(false);
-
-    const scale = useMemo(
-        (): AnimatedInterpolation => scrollY.interpolate(interpolateConfig),
-        [scrollY]
-    );
+    const { scale, ref, onScroll, onScrollBeginDrag, onScrollEndDrag } =
+        useProfileScrollView();
 
     return (
         <Animated.ScrollView
-            ref={listRef}
+            ref={ref}
             showsVerticalScrollIndicator={false}
             scrollEventThrottle={16}
             onScroll={onScroll}
---
+++ src/hooks/useProfileScrollView.ts
@@ -0,0 +1,121 @@
+import { Ref, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import {
+    Animated,
+    NativeScrollEvent,
+    NativeSyntheticEvent,
+    ScrollView
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { animatedEventConfig } from '@components/profile/ProfileScrollView/ProfileScrollView.config';
+import { setBottomBarVisible } from '@store/BottomBarReducer';
+import { interpolateConfig } from '@components/profile/ProfileAnimatedImage/ProfileAnimatedImage.config';
+import AnimatedInterpolation = Animated.AnimatedInterpolation;
+
+export const useProfileScrollView = (): {
+    scale: AnimatedInterpolation;
+    ref: Ref<ScrollView>;
+    onScroll: (event: NativeSyntheticEvent<NativeScrollEvent>) => void;
+    onScrollBeginDrag: () => void;
+    onScrollEndDrag: () => void;
+} => {
+    const dispatch = useDispatch();
+
+    const [lastContentOffset, setLastContentOffset] = useState<number>(0);
+    const [isScrolling, setIsScrolling] = useState<boolean>(false);
+    const [isBottomBarVisible, setIsBottomBarVisible] = useState<boolean>(true);
+    const [canImageInterpolate, setCanImageInterpolate] =
+        useState<boolean>(false);
+
+    const scrollY = useRef(new Animated.Value(0)).current;
+    const ref = useRef(null);
+
+    useEffect(() => {
+        dispatch(setBottomBarVisible(isBottomBarVisible));
+    }, [dispatch, isBottomBarVisible]);
+
+    const scale = useMemo(
+        (): AnimatedInterpolation => scrollY.interpolate(interpolateConfig),
+        [scrollY]
+    );
+
+    const isScrollingDown = useCallback(
+        (offset: number, y: number) => offset > y,
+        []
+    );
+
+    const onScroll = useMemo(
+        (): ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) =>
+            Animated.event(
+                [
+                    {
+                        nativeEvent: {
+                            contentOffset: { y: scrollY }
+                        }
+                    }
+                ],
+                {
+                    ...animatedEventConfig,
+                    listener: (
+                        event: NativeSyntheticEvent<NativeScrollEvent>
+                    ) => {
+                        const isDown = isScrollingDown(
+                            lastContentOffset,
+                            event.nativeEvent.contentOffset.y
+                        );
+
+                        if (
+                            isDown &&
+                            event.nativeEvent.contentOffset.y <= 0 &&
+                            canImageInterpolate &&
+                            !isScrolling
+                        ) {
+                            ref.current?.scrollTo({
+                                x: 0,
+                                y: 0,
+                                animated: false
+                            });
+                            setCanImageInterpolate(false);
+                        }
+
+                        if (isDown && event.nativeEvent.contentOffset.y < 150) {
+                            setIsBottomBarVisible(true);
+                        } else if (
+                            !isDown &&
+                            event.nativeEvent.contentOffset.y >= 0 &&
+                            isScrolling
+                        ) {
+                            setIsBottomBarVisible(false);
+                        }
+
+                        if (
+                            !isDown &&
+                            event.nativeEvent.contentOffset.y > 100
+                        ) {
+                            setCanImageInterpolate(true);
+                        }
+
+                        setLastContentOffset(event.nativeEvent.contentOffset.y);
+                    }
+                }
+            ),
+        [
+            canImageInterpolate,
+            isScrolling,
+            isScrollingDown,
+            lastContentOffset,
+            scrollY
+        ]
+    );
+
+    const onScrollBeginDrag = () => setIsScrolling(true);
+
+    const onScrollEndDrag = () => setIsScrolling(false);
+
+    return {
+        scale,
+        ref,
+        onScroll,
+        onScrollBeginDrag,
+        onScrollEndDrag
+    };
+};
