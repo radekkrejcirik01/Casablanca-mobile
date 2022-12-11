3ad96bf66a62d7c4976051c03565cf28c474c12e
radekkrejcirik01 <radek.krejcirik@cloudtalk.io>
2022-12-11 14:56:14 +0100

    feat: render DotProgressBar dots only when more than one photo

--- src/components/general/DotProgressBar/DotProgressBar.tsx
+++ src/components/general/DotProgressBar/DotProgressBar.tsx
@@ -12,26 +12,29 @@ export const DotProgressBar = ({
     currentPage,
     style
 }: DotProgressBarProps): JSX.Element => {
-    const pageArray = [...Array(pagesNumber).keys()];
+    if (pagesNumber > 1) {
+        const pageArray = [...Array(pagesNumber).keys()];
 
-    const dotColor = useCallback(
-        (value: number): StyleProp<ViewStyle> => ({
-            backgroundColor:
-                value === currentPage ? COLORS.WHITE : COLORS.LIGHTGRAY_300
-        }),
-        [currentPage]
-    );
+        const dotColor = useCallback(
+            (value: number): StyleProp<ViewStyle> => ({
+                backgroundColor:
+                    value === currentPage ? COLORS.WHITE : COLORS.LIGHTGRAY_300
+            }),
+            [currentPage]
+        );
 
-    return (
-        <View style={[DotProgressBarStyle.container, style]}>
-            {pageArray.map((value: number) => (
-                <View
-                    key={value}
-                    style={[DotProgressBarStyle.dot, dotColor(value)]}
-                />
-            ))}
-        </View>
-    );
+        return (
+            <View style={[DotProgressBarStyle.container, style]}>
+                {pageArray.map((value: number) => (
+                    <View
+                        key={value}
+                        style={[DotProgressBarStyle.dot, dotColor(value)]}
+                    />
+                ))}
+            </View>
+        );
+    }
+    return null;
 };
 
 DotProgressBar.defaultProps = DotProgressBarDefaultProps;
