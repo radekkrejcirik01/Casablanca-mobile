62b8c99db1fa03e0a99dd2f4cb89b4b36469771f
radekkrejcirik01 <radek.krejcirik@cloudtalk.io>
2022-12-27 14:48:54 +0100

    feat: useNotifications, navigationRef

---
+++ src/hooks/useNotifications.ts
@@ -0,0 +1,55 @@
+import { useCallback, useEffect } from 'react';
+import messaging, {
+    FirebaseMessagingTypes
+} from '@react-native-firebase/messaging';
+import { NavigationContainerRefWithCurrent } from '@react-navigation/native';
+import { useFlashMessage } from '@hooks/useFlashMessage';
+import { BottomTabNavigatorEnum } from '@navigation/BottomTabNavigator/BottomTabNavigator.enum';
+
+export const useNotifications = (
+    navigationRef: NavigationContainerRefWithCurrent<ReactNavigation.RootParamList>,
+    isReady: boolean
+) => {
+    const displayMessage = useFlashMessage();
+
+    const navigateToMessages = useCallback(() => {
+        navigationRef?.current?.navigate(
+            BottomTabNavigatorEnum.MessagesTab as never
+        );
+    }, [navigationRef]);
+
+    useEffect(() => {
+        if (isReady) {
+            // On open notification from killed state
+            messaging()
+                .getInitialNotification()
+                .then((remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+                    if (remoteMessage) {
+                        navigateToMessages();
+                    }
+                });
+
+            // On open notification from background state
+            messaging().onNotificationOpenedApp(
+                (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+                    if (remoteMessage) {
+                        navigateToMessages();
+                    }
+                }
+            );
+
+            // On notification comes when being in app
+            messaging().onMessage(
+                (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+                    if (remoteMessage) {
+                        displayMessage(
+                            remoteMessage.notification.title,
+                            remoteMessage.notification.body,
+                            () => navigateToMessages()
+                        );
+                    }
+                }
+            );
+        }
+    }, [displayMessage, isReady, navigateToMessages]);
+};
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,21 @@
-import React, { useMemo } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useMemo, useState } from 'react';
+import {
+    createNavigationContainerRef,
+    NavigationContainer
+} from '@react-navigation/native';
 import { DARK_THEME, LIGHT_THEME } from '@navigation/navigation.const';
 import { useTheme } from '@hooks/useTheme';
+import { useNotifications } from '@hooks/useNotifications';
 import { RootStackNavigator } from './RootNavigator/RootStackNavigator';
 
 export const Navigation = (): JSX.Element => {
+    const navigationRef = createNavigationContainerRef();
+    const [isReady, setIsReady] = useState<boolean>(false);
+
+    useNotifications(navigationRef, isReady);
     const { isDarkMode } = useTheme();
+
+    const onReady = () => setIsReady(true);
 
     const theme = useMemo(
         () => (isDarkMode ? DARK_THEME : LIGHT_THEME),
@@ -13,7 +23,11 @@ export const Navigation = (): JSX.Element => {
     );
 
     return (
-        <NavigationContainer theme={theme}>
+        <NavigationContainer
+            ref={navigationRef}
+            onReady={onReady}
+            theme={theme}
+        >
             <RootStackNavigator />
         </NavigationContainer>
     );
